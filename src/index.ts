413e901ec9426d03042b5ac7699de30193e6e2a3
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-08-14 15:47:13 +0100

    chore: update import paths for SillyTavern-Extension-ZerxzLib

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 
 async function importFromScript(path: string) {
-    const root = "../../../../../";
-    return await import(/* webpackIgnore: true */ root + path) ;
+    const root = "../../../../";
+    return await import(/* webpackIgnore: true */ root + path);
 }
 console.log("Hello from SillyTavern-Extension-ZerxzLib!")
 console.log("Importing from SillyTavern-Extension-ZerxzLib/src/index.ts")
 
-async  function init(){
+async function init() {
     console.log("init");
     const script = await importFromScript("script.js")
     console.log(script);
-    const {getContext} = await importFromScript("script/extensions.js")
+    const { getContext } = await importFromScript("script/extensions.js")
     console.log(getContext());
 }
 init();
