12e8cae1534c06330324e408bad5cd826960c806
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-08-16 00:27:53 +0100

    chore: Update webpack configuration for chunk splitting and vendor extraction

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,7 @@ module.exports = {
         minimize: true,
         minimizer: [new TerserPlugin({ extractComments: false })],
         splitChunks:{
+            chunks: 'all',
             minSize: 20000,
             maxSize: 0,
             minChunks: 1,
@@ -77,7 +78,7 @@ module.exports = {
                     },
                     test: /[\\/]node_modules[\\/]/,
                     priority: -10,
-                    chunks: 'async',
+                    enforce: true,
                 },
                 default: {
                     name: 'default',
