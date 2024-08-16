665279923bc2c301cd6627ce75209f8d4dc20bde
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-08-16 02:01:13 +0100

    chore: Optimize code for iterating over lib object and executing functions

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 import * as lib from './lib.js';
-for(let key in lib) {
-  console.log(key);
-  console.log(lib[key]);
-  lib[key]();
+for(const key in lib) {
+    console.log(key);
+    console.log(lib[key]);
+    const value = lib[key];
+    if (typeof value === 'function')value()
+    else if (typeof value === 'object') {
+        typeof value.default == 'function' && value.default();
+    }
 }
