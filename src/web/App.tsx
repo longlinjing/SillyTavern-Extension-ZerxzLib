4c470715c73002553b8b00be9d2c3fdb3039b39a
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-08-16 15:39:15 +0100

    chore: Update App component to include a slide-in alert dialog

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,23 +1,62 @@
 /* global SillyTavern */
-
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 // eslint-disable-next-line no-var
 function App() {
     function handleClick() {
         alert(`Hello, ${SillyTavern.getContext().name1}!`);
     }
+    const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
 
     return (
-        <Stack>
-            <Button className="menu_button" onClick={() => handleClick()}>
-                Click me
-            </Button>
-            <Button className="menu_button" onClick={() => handleClick()}>
-                Click me
-            </Button>
-        </Stack>
+    <React.Fragment>
+      <Button <React.Fragment> >
+        Slide in alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose}>Agree</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
 
     );
 }
