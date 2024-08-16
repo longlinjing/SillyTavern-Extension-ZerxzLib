14511fab6860f25659942b6f915f90feec313489
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-08-16 23:48:07 +0100

    chore: Update webpack configuration to enable lazy loading of sandboxjs library module

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,23 +1,28 @@
 /* global SillyTavern */
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import {Fab} from "@mui/material";
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import { TransitionProps } from '@mui/material/transitions';
 import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import Box from '@mui/material/Box';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
 
-const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & {
-    children: React.ReactElement<any, any>;
-  },
-  ref: React.Ref<unknown>,
-) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
+const actions = [
+    { icon: <FileCopyIcon />, name: 'Copy' },
+    { icon: <SaveIcon />, name: 'Save' },
+    { icon: <PrintIcon />, name: 'Print' },
+    { icon: <ShareIcon />, name: 'Share' },
+];
 // eslint-disable-next-line no-var
 function App() {
     function handleClick() {
@@ -35,17 +40,32 @@ function App() {
 
 
     return (
-        <ScopedCssBaseline>
+        <ScopedCssBaseline style={{
+            display: 'block',
+        }} >
 
-      <Button  variant="outlined" onClick={handleClickOpen}>
-        Slide in alert dialog
-      </Button>
+            <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 ,display:"block"}}>
+                <SpeedDial
+                    ariaLabel="SpeedDial basic example"
+                    sx={{ position: 'fixed', bottom: 16, right: 16,zIndex: 2147483647 ,display: 'flex', flexDirection: 'column-reverse', alignItems: 'flex-end' }}
+                    icon={<SpeedDialIcon />}
+                >
+                    {actions.map((action) => (
+                        <SpeedDialAction
+                            key={action.name}
+                            icon={action.icon}
+                            tooltipTitle={action.name}
+                            onClick={handleClickOpen}
+                        />
+                    ))}
+                </SpeedDial>
+            </Box>
       <Dialog
+
         open={open}
-        TransitionComponent={Transition}
-        keepMounted
         onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
+        keepMounted
+        sx={{display: 'block',zIndex: 2147483647,position:'absolute'}}
       >
         <DialogTitle>{"Use Google's location service?"}</DialogTitle>
         <DialogContent>
--- src/web/index.tsx
+++ src/web/index.tsx
@@ -12,11 +12,12 @@ import '@fontsource/roboto/700.css';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export default () => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const rootContainer = document.body!;
+    const rootContainer = document.body;
     const rootElement = document.createElement('div');
-    rootElement.id = 'zerxz-lib-root';
-    rootElement.style = 'all: initial,position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: black; color: white; padding: 20px; border: 1px solid white; z-index: 10000; display: block; overflow-y: auto; max-height: 80vh;';
     rootContainer.appendChild(rootElement);
+    rootElement.id = 'zerxz-lib-root';
+    rootElement.setAttribute('style', 'all: initial;position: absolute;bottom: 0;right: 0;z-index: 2147483647;')
+
     console.log('rootContainer', rootContainer);
     console.log('rootElement', rootElement);
     const root = ReactDOM.createRoot(rootElement);
